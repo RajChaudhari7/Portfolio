7673e92255c6be483efa8c3329a1646ea06bd8c8
Raj Chaudhari <rajchaudharii1403@gmail.com>
2025-12-29 11:19:30 +0530

    Created Contact and Footer

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -50,7 +50,7 @@ const Hero = () => {
             </div>
 
             <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
-              Hi, I’m Adrian, a developer based in Croatia with a passion for
+              Hi, I’m Raj Chaudhari, a developer based in MERN with a passion for
               code.
             </p>
 
